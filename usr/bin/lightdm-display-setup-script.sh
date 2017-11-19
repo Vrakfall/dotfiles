4eccdd390c327163e8813a959aeb987af2cdd64e
Jérémy "Vrakfall" Lecocq <jeremy@artphotolaurent.be>
2017-11-20 00:07:12 +0100

    Fix xrandr initial setup for when displays change name. It will not use the first HDMI one as primary and the first DVI at its right, regardless of their name.

--- usr/bin/lightdm-display-setup-script.sh
+++ usr/bin/lightdm-display-setup-script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-xrandr --output HDMI-4 --primary --output DVI-I-1 --right-of HDMI-4
+
+# Get the first HDMI display
+DISPLAY_HDMI0=$(xrandr -q | sed -n 's/\(^HDMI-[0-9]\(-[0-9]\)\?\) connected.*/\1/p')
+# Get the first DVI display
+DISPLAY_DVI0=$(xrandr -q | sed -n 's/\(^DVI-I\(-[0-9]\)\?\) connected.*/\1/p')
+
+# Place the displays correctly
+# TODO Place the DVI one a bit lower
+xrandr --output $DISPLAY_HDMI0 --primary --output $DISPLAY_DVI0 --right-of $DISPLAY_HDMI0
 
 setxkbmap be
 test -f /usr/share/X11/xkb/keymaps/xorg/zboard-merc-part1-normal.xkb && xkbcomp -i 11 /usr/share/X11/xkb/keymaps/xorg/zboard-merc-part1-normal.xkb $DISPLAY
 test -f /usr/share/X11/xkb/keymaps/xorg/zboard-merc-part2-normal-no-caps-lock.xkb && xkbcomp -i 12 /usr/share/X11/xkb/keymaps/xorg/zboard-merc-part2-normal-no-caps-lock.xkb $DISPLAY
-touch /home/vrakfall/ping
